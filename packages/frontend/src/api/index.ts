698e5013694f12ff546136e897b0ac8a57cf2498
Kail Keusch <kail.keusch@gmail.com>
2025-03-29 00:18:05 -0400

    Try again

--- packages/frontend/src/api/index.ts
+++ packages/frontend/src/api/index.ts
@@ -1,3 +1,2 @@
 export const API_URL =
-  process.env.REACT_APP_API_URL ||
-  "https://shielded-garden-58332-d715cbe2ddeb.herokuapp.com/";
+  "https://shielded-garden-58332-d715cbe2ddeb.herokuapp.com";
