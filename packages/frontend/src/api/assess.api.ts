50d581af14dd389039d27a21715799e99f1ff491
Kail Keusch <kail.keusch@gmail.com>
2025-03-28 23:57:42 -0400

    Start setting up deploy

--- packages/frontend/src/api/assess.api.ts
+++ packages/frontend/src/api/assess.api.ts
@@ -1,7 +1,8 @@
 import { Answer } from "@/contexts/SectionContext";
+import { API_URL } from "./index";
 
 export const assessAnswers = async (answers: Answer[]) => {
-  const response = await fetch("http://localhost:3000/api/assessment/assess", {
+  const response = await fetch(`${API_URL}/api/assessment/assess`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
---
+++ packages/frontend/src/api/index.ts
@@ -0,0 +1,3 @@
+export const API_URL =
+  process.env.REACT_APP_API_URL ||
+  "https://shielded-garden-58332-d715cbe2ddeb.herokuapp.com/";
