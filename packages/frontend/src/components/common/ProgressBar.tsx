43244dd66b921ad5e83d42cb7b8e0753faa8036f
Kail Keusch <kail.keusch@gmail.com>
2025-03-28 21:27:46 -0400

    Add text progress to ProgressBar

--- packages/frontend/src/components/common/ProgressBar.tsx
+++ packages/frontend/src/components/common/ProgressBar.tsx
@@ -7,12 +7,17 @@ function ProgressBar({ current, total }: ProgressBarProps) {
   const percentage = (current / total) * 100;
 
   return (
-    <div className="w-full bg-gray-200 rounded-full h-2.5">
-      <div
-        className="bg-blue-600 h-2.5 rounded-full transition-all duration-300"
-        style={{ width: `${percentage}%` }}
-      />
-    </div>
+    <>
+      <div className="text-xs font-medium uppercase text-secondary-600 mb-2">
+        Question {current} of {total}
+      </div>
+      <div className="w-full bg-gray-200 rounded-full h-2.5">
+        <div
+          className="bg-blue-600 h-2.5 rounded-full transition-all duration-300"
+          style={{ width: `${percentage}%` }}
+        />
+      </div>
+    </>
   );
 }
 
